e1cdaf194db764bb219341f52b5fec92cf67ca54
senhorAfonso <sirafonso005896@outlook.com>
2023-11-18 14:46:20 -0300

    ::sparkles: <feat>(controller/auth) token creation implemented

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,6 +1,7 @@
 
 import { type Request, type Response } from 'express';
 import userModel from '../model/userModel';
+import jwt from 'jsonwebtoken';
 
 // criar token
 // tratar erros unique e validation
@@ -8,10 +9,10 @@ import userModel from '../model/userModel';
 
 const registerNewUser = async (req: Request, res: Response) => {
   const user = req.body;
-  console.log(user);
+  const token = jwt.sign({user: user.name}, 'secreteKey', {expiresIn: '30d'});
   const newUser = await userModel.create(req.body);
 
-  res.send(newUser);
+  res.send({token, newUser});
 };
 
 export {
